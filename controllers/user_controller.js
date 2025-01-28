52437ec2a22acb32fcb56dc8fd8b0f381854afe0
alirazagpt4 <ali.4aug24webgpt@gmail.com>
2025-01-28 23:18:53 +0500

    change in API of SignUp and Login

--- controllers/user_controller.js
+++ controllers/user_controller.js
@@ -1,48 +1,51 @@
-
 const JWT_SECRET = process.env.JWT_SECRET;
-import User from '../models/user_model.js';
-import jwt from 'jsonwebtoken';
+import User from "../models/user_model.js";
+import jwt from "jsonwebtoken";
+import bcrypt from "bcrypt";
 
-
-
-export const  signup = async (req , res)=>{
-    try{
-         const {name , email , password} = req.body;
-          let newUser = await User.create([
-             {name , email , password}
-            ]);
-         res.status(201).json({message:"signup Successfully" , newUser});
-
+export const signup = async (req, res) => {
+  try {
+    const { name, email, password } = req.body;
+    const hashedPassword = await bcrypt.hash(password, 10);
+    // Check if user already exists
+    const existingUser = await User.findOne({ email});
+    if(existingUser){
+        return res.status(400).json({message:"User already exists"});
     }
-    catch(err){
-        res.status(500).send("internal server error");
+    let newUser = await User.create([{ name, email, password:hashedPassword }]);
+    res.status(201).json({ message: "signup Successfully", newUser });
+  } catch (err) {
+    res.status(500).send("internal server error");
+  }
+};
+
+
+export const login = async (req, res) => {
+  try {
+    const { email, password } = req.body;
+    const user = await User.findOne({ email });
+    if(!user){
+            return res.status(401).json({message:"User Not Found"})
     }
-}
-export const login = async (req , res)=>{
-    try{
-        const {email , password} = req.body;
-       const user =  await User.find({email , password});
-       const token = jwt.sign({id:user._id , email:user.email} , JWT_SECRET);
-       res.status(200).json({message:"login Successfully" , user , token});
 
+    const isMatch = await bcrypt.compare(password, user.password);
+    if (!isMatch) {
+      return res.status(401).json({ message: "Invalid credentials!" });
     }
-    catch(err){
-        res.status(500).send("internal server error");
-    }
-}
+    const token = jwt.sign({ id: user._id, email: user.email }, JWT_SECRET);
+    res.status(200).json({ message: "login Successfully", user, token });
+  } catch (err) {
+    res.status(500).send("internal server error");
+  }
+};
 
-
-export const authenticationToken = (req , res , next)=>{
-       const token  = req.headers.token;
-       if(token){
-        const decode = jwt.verify(token , JWT_SECRET);
-        req.user = decode;
-        next();
-       }
-       else{
-        res.status(401).json({message:"Token is not provided"});
-       }
-}
-
-
-
+export const authenticationToken = (req, res, next) => {
+  const token = req.headers.token;
+  if (token) {
+    const decode = jwt.verify(token, JWT_SECRET);
+    req.user = decode;
+    next();
+  } else {
+    res.status(401).json({ message: "Token is not provided" });
+  }
+};
--- index.js
+++ index.js
@@ -1,4 +1,7 @@
 import express from 'express';
+import morgan from 'morgan';
+import cors   from 'cors';
+import helmet from 'helmet';
 import routes from './routes/user_routes.js'
 // console.log(express());
 
@@ -8,6 +11,9 @@ const port = 5000;
 
 app.use(express.json());
 app.use(express.urlencoded({extended: true}));
+app.use(morgan('dev'));
+app.use(helmet())
+app.use(cors());
 app.use('/api' , routes)
 
 
